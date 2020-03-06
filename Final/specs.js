cef804784d3e073e7cdc7e351874287db9aa1c5d
sinaladel <oiderem8@gmail.com>
2020-03-06 09:31:00 -0700

    Kept going with product specs

---
+++ Final/specs.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#sampleButton').on('click', function () {
+       window.open("https://docs.google.com/forms/d/e/1FAIpQLSfubM6j3i3CdxS0S2n7jZ63zNqSaqNevtmKOiwnArURQBeOag/viewform?usp=sf_link", "_new")
+    });
+    $('.button').css({
+        "background-color": "lightgrey",
+        "border": "2px solid #000",
+        "width": "100px",
+        "text-align": "center"
+    })
+
+
+
+});
